ad4283e8f263bdec4855e1973aa5f9f60cc4ff02
William Lorand <bill@bigstar3.local>
2019-04-02 15:51:03 -0700

    added PORT var

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 
 const express = require('express');
 const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -10,4 +13,4 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(5000);
+app.listen(PORT);
