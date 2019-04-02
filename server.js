316187cfd69f540ca62122882273222c7cf4b141
William Lorand <bill@bigstar3.local>
2019-04-02 16:21:49 -0700

    added heroku postbuild script

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-// quick express server to serve static index.html
+/**
+ * File: server.js
+ * Desc: quick-n-dirty express server to serve your static react app
+ */
 
 const express = require('express');
 const path = require('path');
 
-const PORT = process.env.PORT || 5000;
-
+const port = process.env.PORT || 5000;
 const app = express();
 
+// if (process.env.NODE_ENV === 'production') {
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+// }
 
-app.listen(PORT);
+app.listen(port, () => console.log(`Server started on port: ${port}`));
