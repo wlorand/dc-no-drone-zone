192b6ee13db8bdddf96624f9e89db869323c54cd
William Lorand <bill@bigstar3.local>
2019-04-02 15:24:18 -0700

    added quick express server and nodemon to serve it via npm script

---
+++ server.js
@@ -0,0 +1,13 @@
+// quick express server to serve static index.html
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(5000);
